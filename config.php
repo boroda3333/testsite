5bbb24646bfe7193ec00d5091cc2530c9e93f4c2
boroda3333 <specialforapex222@gmail.com>
2024-07-14 08:39:01 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://test7/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://test7/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/OSPanel/home/test7/catalog/');
+define('DIR_SYSTEM', 'D:/OSPanel/home/test7/system/');
+define('DIR_IMAGE', 'D:/OSPanel/home/test7/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'MySQL-5.7');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencartparts777');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
